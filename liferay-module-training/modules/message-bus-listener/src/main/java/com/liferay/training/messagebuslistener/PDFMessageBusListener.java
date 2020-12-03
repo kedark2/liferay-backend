984cb1afeb88ed7370d51057d32c6da7fa72db46
kedark2 <kedar.kanel@gmail.com>
2020-12-03 11:26:09 +0200

    created documents and media message bus listener . message-bus-listener module

---
+++ liferay-module-training/modules/message-bus-listener/src/main/java/com/liferay/training/messagebuslistener/PDFMessageBusListener.java
@@ -0,0 +1,35 @@
+package com.liferay.training.messagebuslistener;
+
+import com.liferay.portal.kernel.messaging.DestinationNames;
+import com.liferay.portal.kernel.messaging.Message;
+import com.liferay.portal.kernel.messaging.MessageListener;
+import com.liferay.portal.kernel.messaging.MessageListenerException;
+import com.liferay.portal.kernel.repository.model.FileVersion;
+
+import org.osgi.service.component.annotations.Component;
+
+/**
+ * @author Kedar
+ */
+@Component(
+	immediate = true,
+	property = {"destination.name=" + DestinationNames.DOCUMENT_LIBRARY_PDF_PROCESSOR
+		// TODO enter required service properties
+	},
+	service = MessageListener.class
+)
+public class PDFMessageBusListener implements MessageListener {
+
+	@Override
+	public void receive(Message message) throws MessageListenerException {
+		
+		Object[] payload = (Object[]) message.getPayload();
+		
+		FileVersion fileVersion = (FileVersion)payload[1];
+		
+		System.out.println("Title=" + fileVersion.getTitle());
+	}
+
+	// TODO enter required service methods
+
+}
